42272f2e9dc5be5452df30b1774cac5bce0eb523
Zac Young <zacyoung712@gmail.com>
2019-04-03 08:11:43 +0000

    End of week 6  lecture nothing changed

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
+//Alaphabet just for own refernce to help understand rotation +2
+//abcdefghijklmnopqrstuvwxyz
+//zyabcdefghijklmnopqrstuvwx
 int main()
 {
-printf("107\n");
+int a, b, c;
+scanf("%d %d %d", &a, &b, &c);
+printf("%d %d %d\n", a, b, c);
 }
 
