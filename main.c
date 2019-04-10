71a1e39d0a26434b5512281fa64e038c747f1929
Zac Young <zacyoung712@gmail.com>
2019-04-10 09:26:51 +0000

    Still working on the program, stared using code used in lab 5 task 3

--- main.c
+++ main.c
@@ -4,9 +4,33 @@
 //abcdefghijklmnopqrstuvwxyz
 //zyabcdefghijklmnopqrstuvwx
 int main()
-{
-int a, b, c;
-scanf("%d %d %d", &a, &b, &c);
-printf("%d %d %d\n", a, b, c);
+{ 
+int alpha(abcdefghijklmnopqrstuvwxyz);
+int newaplha(zabcdefhijklmnopqrstuvwxy);
+float n;
+n = sizeof(alpha)/sizeof(alpha[0]);
+int i;
+float max= -1e30 ;
+int maxindex ;
+
+for(i=0; i<=n-1; i++){
+    
+    if(data[i]> max){
+        max = data[i];
+        maxindex = i;
+    }
+    
+}
+    printf("the maxium value for %f, found at %d", max, maxindex);
+        
+        
+ 
+ 
+ 
+ 
+ 
+    
+
+
 }
 
